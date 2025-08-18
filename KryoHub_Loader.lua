f2d5d225e9f813ddad47282b3247cbc6b98ff13e
SirMiloRBLX <enriquejoaquingreyes10@gmail.com>
2025-08-18 23:10:59 +0800

    Create KryoHub_Loader.lua

---
+++ KryoHub_Loader.lua
@@ -0,0 +1,22 @@
+local Supported = {
+    [4623386862] = "https://raw.githubusercontent.com/SirMiloRBLX/Kryo-Scripts/refs/heads/main/KryoHub_Piggy.lua", -- Piggy
+    [5661005779] = "https://raw.githubusercontent.com/SirMiloRBLX/Kryo-Scripts/refs/heads/main/KryoHub_Piggy.lua" -- Piggy: Book 2
+}
+
+pcall(function()
+    local scriptUrl = Supported[game.PlaceId]
+    
+    if scriptUrl then
+        local success, result = pcall(function()
+            return loadstring(game:HttpGet(scriptUrl))()
+        end)
+
+        if success then
+            print("[Kyro Hub] Script loaded successfully for PlaceId:", game.PlaceId)
+        else
+            warn("[Kyro Hub] Failed to execute script:", result)
+        end
+    else
+        warn("[Kyro Hub] This game is not supported. PlaceId:", game.PlaceId)
+    end
+end)
